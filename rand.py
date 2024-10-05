2e0e09a90058b6dc81b8e1cdbc58357a99d7a1c9
a.Kuharenko <you@example.com>
2024-10-05 21:36:00 +0100

    README.md

--- rand.py
+++ rand.py
@@ -1,14 +1,11 @@
 import random
 a = 0 
 b = 0 
-c = 0 
 i = 0
-while i < 10000:
-    a = a + random.randint(1,3) 
-    b = b + random.randint(1,3) 
-    c = c + random.randint(1,3)
+while i < 1000:
+    a = a + random.randint(1,2) 
+    b = b + random.randint(1,2)     
     i += 1
 
 print("a: ", a)
 print("b: ", b)
-print("c: ", c)
